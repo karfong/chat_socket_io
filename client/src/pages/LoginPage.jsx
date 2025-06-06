4b7153d5f2729fe7bc6374db6eb2f54ecd743452
Fong <chankarfong8888@gmail.com>
2025-06-06 23:02:41 +0800

    add picture to the loginpage and add class for div

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import assets from "../assets/assets";
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+      {/* left */}
+      <img src={assets.log_big} alt="" />
     </div>
   );
 };
